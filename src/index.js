64b06d5972aaae05eba342341b5fa856cf82916e
namkijoo <rlwn5904@naver.com>
2023-07-17 16:42:26 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter,Router,Routes,Route,Link,useParams } from 'react-router-dom';
+import Login from './Login/login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/a' element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+  
+)
 
