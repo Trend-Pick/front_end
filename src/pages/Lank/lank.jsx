b4cb7d0e4200bed71fada47de7aac2adcfbd67da
chestnut1044 <wbk1620@gmail.com>
2023-07-19 19:26:14 +0900

    Feat : lank 마크업

--- src/pages/Lank/lank.jsx
+++ src/pages/Lank/lank.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import Header from '../../components/Header/header';
-import Navbar from '../../components/Navbar/navbar';
+import React from "react";
+import Header from "../../components/Header/header";
+import Navbar from "../../components/Navbar/navbar";
+import { AiFillCaretDown } from "react-icons/ai";
+import styles from "./lank.module.css";
 
 export default function Lank() {
   return (
-    <lank>
-      <Header></Header>
+    <lank className={styles.lank}>
+      <Header
+        type={"lank"}
+        title={"월간랭킹"}
+        text={`${"7"}월 첫째주 랭킹`}
+        buttons={<AiFillCaretDown />}
+      ></Header>
       <div>랭킹</div>
       <Navbar></Navbar>
     </lank>
