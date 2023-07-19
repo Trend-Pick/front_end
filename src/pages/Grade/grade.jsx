adff491012bdc16a544cd6574ea4fa853d586d1b
chestnut1044 <wbk1620@gmail.com>
2023-07-19 19:25:42 +0900

    Feat : grade 마크업

--- src/pages/Grade/grade.jsx
+++ src/pages/Grade/grade.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import styles from "./grade.module.css";
 import Header from "../../components/Header/header";
 import Navbar from "../../components/Navbar/navbar";
 
 export default function Grade() {
   return (
-    <div>
+    <grade className={styles.grade}>
       <Header
         type={"grade"}
-        text={"스타일 평가"}
-        summary={"스타일을 평가해보세요"}
+        title={"스타일 평가"}
+        text={"스타일을 평가해보세요!"}
       ></Header>
+      <div>카드</div>
       <Navbar></Navbar>
-    </div>
+    </grade>
   );
 }
