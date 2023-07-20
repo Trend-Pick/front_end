422f8237ac891635a3f019c10b210e055d705c95
chestnut1044 <wbk1620@gmail.com>
2023-07-21 05:43:31 +0900

    Feat : profile, board 추가

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import Login from "./pages/Login/login";
 import Lank from "./pages/Lank/lank";
 import Grade from "./pages/Grade/grade";
 import Register from "./pages/Register/register";
+import Board from "./pages/Board/board";
+import Profile from "./pages/Profile/profile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -24,6 +26,8 @@ root.render(
         <Route path="/Lank" element={<Lank />}></Route>
         <Route path="/Grade" element={<Grade />}></Route>
         <Route path="/Register" element={<Register />}></Route>
+        <Route path="/Board" element={<Board />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
