9dac222cb32e42d82764081182407cda5967a774
chestnut1044 <wbk1620@gmail.com>
2023-07-21 17:03:45 +0900

    Feat : 게시판 헤더/네비바 추가

--- src/pages/Board/board.jsx
+++ src/pages/Board/board.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Header from "../../components/Header/header";
 import Navbar from "../../components/Navbar/navbar";
+import styles from './board.module.css'
 
 export default function Board() {
   return (
-    <board>
+    <boar className={styles.board}d>
       <Header
         type={"board"}
         title={"자유게시판"}
       ></Header>
       <div>자유게시판</div>
       <Navbar></Navbar>
-    </board>
+    </boar>
   );
 }
