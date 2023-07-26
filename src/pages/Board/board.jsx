87c730ca43eb96399d89f7ef27ef774f38264058
namkijoo <rlwn5904@naver.com>
2023-07-27 04:01:44 +0900

    Feat:사진업로드

--- src/pages/Board/board.jsx
+++ src/pages/Board/board.jsx
@@ -16,6 +16,7 @@ export default function Board() {
   },[])
   return (
     <div className={styles.board}>
+      
       <Header
         id={styles.header}
         type={"board"}
@@ -25,9 +26,25 @@ export default function Board() {
         {boardList.map((item)=>{
           return(
             <div id={styles.boardList} key={item.id}>
-              <div id={styles.title}>{item.roomName}</div>
-              <span id={styles.date}>닉네임/현재시간-작성시간</span>
-              <div id={styles.content}>{item.content.length>45?item.content.slice(0, 45) + "...":null}</div>
+              {item.image==null?
+              <div>
+                <div id={styles.title1}>{item.roomName}</div>
+                <span id={styles.date1}>닉네임/현재시간-작성시간</span>
+                <div id={styles.content1}>{item.content.length>45?item.content.slice(0, 45) + "...":null}</div>
+              </div>
+              :
+              <div id={styles.wrapper2}>
+                <div id={styles.div1}>
+                  <div id={styles.title1}>{item.roomName}</div>
+                  <span id={styles.date1}>닉네임/현재시간-작성시간</span>
+                  <div id={styles.content1}>{item.content.length>20?item.content.slice(0, 20) + "...":null}</div>
+                </div>
+                <div id={styles.div2}>
+                  <img src={item.image}></img>
+                </div>
+              </div>
+              }
+              
             </div>
           )
         })}
