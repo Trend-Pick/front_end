6fe0e4386720995d86a6fc50db34dfed98fdc88f
chestnut1044 <wbk1620@gmail.com>
2023-08-11 20:47:32 +0900

    Feat : header 디자인 수정 / 버튼별 기능 추가

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./header.module.css";
 
-export default function Header({ type, title, text, buttons }) {
-  return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>
-        {title}
-        {type === "lank" ? buttons : null}
-      </h1>
-      <p className={styles.text}>{text}</p>
-    </header>
-  );
+
+// handleBtnClick props로 전달
+export default function Header({ type, title, text, buttons, handleBtnClick }) {
+  if (type == "grade") {
+    return (
+      <header className={styles.header_grade}>
+        <h1 className={styles.title_grade}>{title}</h1>
+        <p className={styles.text}>{text}</p>
+      </header>
+    );
+  } else if (type == "rank") {
+    return (
+      <header className={styles.header_rank}>
+        <h1 className={styles.title_rank} onClick={handleBtnClick}>
+          {title}
+          {buttons}
+        </h1>
+        <p className={styles.text}>{text}</p>
+      </header>
+    );
+  } else if (type == "board") {
+    return (
+      <header className={styles.header_board}>
+        <h1 className={styles.title_board}>{title}</h1>
+        <h1 className={styles.icons_board} onClick={handleBtnClick}>{buttons}</h1>
+      </header>
+    );
+  } else if (type == "profile") {
+    return (
+      <header className={styles.header_profile}>
+        <h1 className={styles.icons_profile} onClick={handleBtnClick}>{buttons}</h1>
+      </header>
+    );
+  }
 }
