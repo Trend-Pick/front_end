5195fb28f98bd3380f09400274385aa13fd39f41
chestnut1044 <wbk1620@gmail.com>
2023-08-16 21:24:12 +0900

    Fix : 100vh 버그 수정

--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  BrowserRouter,
-  Router,
-  Routes,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
-import Login from "./pages/Login/login";
-import Rank from "./pages/Rank/rank";
-import Grade from "./pages/Grade/grade";
-import Register from "./pages/Register/register";
-import Board from "./pages/Board/board";
-import Profile from "./pages/Profile/profile";
-import BoardWrite from "./pages/Board/write";
-import BoardDetail from "./pages/Board/boardDetail";
-import BoardEdit from "./pages/Board/boardEdit";
-import FindPw from "./pages/findPw/findPw";
+
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`); 
-
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/Rank" element={<Rank />}></Route>
-        <Route path="/Grade" element={<Grade />}></Route>
-        <Route path="/Register" element={<Register />}></Route>
-        <Route path="/Board" element={<Board />}></Route>
-        <Route path="/BoardWrite" element={<BoardWrite/>}></Route>
-        <Route path="/BoardDetail" element={<BoardDetail />}></Route>
-        <Route path="/BoardEdit" element={<BoardEdit />}></Route>
-        <Route path="/Profile" element={<Profile />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+root.render(<App />);
