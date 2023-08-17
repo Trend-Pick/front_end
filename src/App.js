99e9ea29a716084513759759ff68101e8a93fb6f
namkijoo <rlwn5904@naver.com>
2023-08-17 20:36:45 +0900

    Feat:오류설정

--- src/App.js
+++ src/App.js
@@ -20,9 +20,6 @@ import BoardEdit from "./pages/Board/boardEdit";
 import { AnimatePresence } from 'framer-motion';
 import { motion } from "framer-motion";
 export default function App() {
-  const AnimatedRoute = ({ path, element }) => (
-    <Route path={path} element={<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.7 }}>{element}</motion.div>} />
-  );
   
   return (
     
--- src/pages/Board/write.jsx
+++ src/pages/Board/write.jsx
@@ -38,7 +38,7 @@ function Write(){
             }
             else{
                 formData.append("createPostRequest",new Blob([JSON.stringify(createPostRequest)], {type:"application/json"}));
-                formData.append("imgInPost",new Blob([JSON.stringify(image)],{type:"multipart/form-data"}));
+                formData.append("imgInPost",image);
             }
             axios.post("/create_post", formData,{headers: {
                 'Content-Type': 'multipart/form-data',
