59d66135df91e4b5078099b0689114359458f830
namkijoo <rlwn5904@naver.com>
2023-08-19 20:06:48 +0900

    Feat:회원가입이메일체크

--- src/pages/Register/register.jsx
+++ src/pages/Register/register.jsx
@@ -11,12 +11,28 @@ function Register(){
     const [pw2,setPw2]=useState("")
     const [nick,setNick]=useState("")
     const [buttonChk,setButtonChk]=useState(true)
+    const [buttonChk2,setButtonChk2]=useState(true)
+    const[emailValid,setEmailValid]  = useState(false);
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
     const changeId=(e)=>{
         setId(e.target.value)
     }
-    const changeEmail=(e)=>{
-        setEmail(e.target.value)
+    const changeEmail=async(e)=>{
+        const newEmail=e.target.value
+        setEmail(newEmail)
+        try {
+            const response = await axios.post("/validation/email", { email: newEmail });
+          if(response.data==true){
+            setEmailValid(true)
+            setButtonChk2(true)
+            }
+            else{
+                setEmailValid(false)
+                setButtonChk2(false)
+            }
+        } catch (error) {
+          console.log(error);
+        }
     }
     const changePw1=(e)=>{
         setPw1(e.target.value)
@@ -42,7 +58,7 @@ function Register(){
             const response = await axios.post("/validation/id", { user_user_id: id });
           console.log(response)
           if(response.data==true){
-            setButtonChk(false);
+            setButtonChk(true);
             alert("아이디가 중복됩니다.")
             setInputChk(false)
           }
@@ -118,17 +134,25 @@ function Register(){
                 <div id={styles.registerForm}>
                     <div id={styles.form}>
                         <span >아이디</span>
-                        <div>
-                            <input onChange={changeId} disabled={inputChk} placeholder="아이디를 입력해주세요"type="text" onKeyDown={onKeyPress}></input>
+                        <div id={styles.idWrap}>
+                            <input id={styles.inputId} onChange={changeId} disabled={inputChk} placeholder="아이디를 입력해주세요"type="text" onKeyDown={onKeyPress}></input>
                             <button onClick={idcheck} type="button">확인</button>
                         </div>
                     </div>
-                    <Form onInputChange={changeEmail} spanName="이메일" placeholder="이메일을 입력해주세요" type="text"></Form>
+                    <div id={styles.form}>
+                        <span >이메일</span>
+                        <div id={styles.emailWrap}>
+                            <input onChange={changeEmail} id={styles.inputEmail} placeholder="이메일 입력해주세요"type="text" ></input>
+                            {emailValid&&email.length>0&&(
+                                <div id={styles.emailCheck}>중복된 이메일 입니다</div>  
+                            )}
+                        </div>
+                    </div>
                     <Form onInputChange={changePw1} spanName="비밀번호" placeholder="특수문자 제외 10자리 이상 입력해주세요." type="text"></Form>
                     <Form onInputChange={changePw2} spanName="비밀번호 확인" placeholder="비밀번호를 다시 입력해주세요" type="text"></Form>
                     <Form onInputChange={changeNick} spanName="닉네임" placeholder="닉네임을 입력해주세요" type="text"></Form>
                 </div>
-                <button onClick={registerBtn} type="button" disabled={buttonChk} id={styles.registerBtn} >회원가입</button>
+                <button onClick={registerBtn} type="button" disabled={buttonChk||buttonChk2} id={styles.registerBtn} >회원가입</button>
             </div>
         </div>
     )
