643508c80ca94e55a6aeb8eb69a65cd073f681b2
chestnut1044 <wbk1620@gmail.com>
2023-08-23 04:05:34 +0900

    Feat : rank 기능

--- src/pages/Rank/rank.jsx
+++ src/pages/Rank/rank.jsx
@@ -1,47 +1,62 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header/header";
 import Navbar from "../../components/Navbar/navbar";
-import { AiFillCaretDown } from "react-icons/ai";
+import Card from "../../components/Card/card";
 import styles from "./rank.module.css";
-
-
+import axios from "axios";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, EffectCreative, EffectCoverflow } from "swiper/modules";
+import { EffectCoverflow } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import Card from "../../components/Card/card";
-
 export default function Rank() {
-  const [toggleRanking, setToggleRanking] = useState("MONTH");
+  const [currentRanking, setCurrentRanking] = useState("월간랭킹");
+  const [cardData, setCardData] = useState([]);
+  const [toggle, setToggle] = useState(false);
+  const now = new Date();
+
+  // 정렬 기준이 바뀔때마다 카드데이터 업데이트
+  useEffect(() => {
+    if (currentRanking == "월간랭킹") {
+      axios.get("monthly_ranking").then((res) => setCardData(res.data));
+    } else if (currentRanking == "주간랭킹") {
+      axios.get("weekly_ranking").then((res) => setCardData(res.data));
+    } else {
+      axios.get("pictures_ranking").then((res) => setCardData(res.data));
+    }
+  }, [currentRanking]);
+
+  // 토글 상태 변경
   const handleChangeToggleBtn = () => {
-    console.log("ㅇㅇ바꿀게")
-  }
-  const cardData = [
-    // 백엔드로부터 데이터 받아오기
-    {
-      imgUrl: require("../../img/KakaoTalk_20210825_045216360.jpg"),
-      grade: 0,
-    },
-    {
-      imgUrl: require("../../img/마멜.jpg"),
-      grade: 0,
-    },
-    {
-      imgUrl: require("../../img/IMG_0019.jpg"),
-      grade: 0,
-    },
-  ];
+    setToggle((prev) => !prev);
+  };
+
+  // 현재 정렬기준 변경
+  const handleChangeCurrentRanking = (data) => {
+    setCurrentRanking(data);
+    setToggle((prev) => !prev);
+  };
 
   return (
     <div className={styles.rank}>
       <Header
         type={"rank"}
-        title={"월간랭킹"}
-        text={`${"7"}월 첫째주 랭킹`}
-        buttons={<AiFillCaretDown />}
+        title={currentRanking}
+        text={
+          currentRanking == "월간랭킹"
+            ? `${now.getMonth() + 1}월 랭킹`
+            : currentRanking == "주간랭킹"
+            ? `${now.getMonth() + 1}월 ${Math.floor(
+                now.getWeek() / (now.getMonth() + 1)
+              )}째주 랭킹`
+            : "누적랭킹을 확인하세요!"
+        }
+        buttons={[<AiFillCaretUp />, <AiFillCaretDown />]}
+        toggle={toggle}
+        handleChangeCurrentRanking={handleChangeCurrentRanking}
         handleBtnClick={handleChangeToggleBtn}
       ></Header>
       <div className={styles.rank_container}>
@@ -50,9 +65,9 @@ export default function Rank() {
           loop={true}
           centeredSlides={true}
           slidesPerView={1.5}
-          loopAdditionalSlides={2}
+          loopadditionalslides={2}
           coverflowEffect={{
-            slideShadows:false,
+            slideShadows: false,
             rotate: 0,
             stretch: 190,
             depth: 10,
@@ -61,18 +76,47 @@ export default function Rank() {
           modules={[EffectCoverflow]}
           className={styles.swiper}
         >
-          {cardData.map((card) => (
-            <SwiperSlide className={styles.card_container}>
+          {cardData.map((card, index) => (
+            <SwiperSlide className={styles.card_container} key = {index}>
               <Card
+                id={index}
                 type={"RANK"}
-                imgUrl={card.imgUrl}
+                cardData={card}
                 className={styles.card}
               />
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
-      <Navbar></Navbar>
+      <Navbar active={"/rank"}></Navbar>
     </div>
   );
 }
+
+
+Date.prototype.getWeek = function (dowOffset) {
+  dowOffset = typeof dowOffset == "number" ? dowOffset : 0;
+  var newYear = new Date(this.getFullYear(), 0, 1);
+  var day = newYear.getDay() - dowOffset;
+  day = day >= 0 ? day : day + 7;
+  var daynum =
+    Math.floor(
+      (this.getTime() -
+        newYear.getTime() -
+        (this.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
+        86400000
+    ) + 1;
+  var weeknum;
+  if (day < 4) {
+    weeknum = Math.floor((daynum + day - 1) / 7) + 1;
+    if (weeknum > 52) {
+      let nYear = new Date(this.getFullYear() + 1, 0, 1);
+      let nday = nYear.getDay() - dowOffset;
+      nday = nday >= 0 ? nday : nday + 7;
+      weeknum = nday < 4 ? 1 : 53;
+    }
+  } else {
+    weeknum = Math.floor((daynum + day - 1) / 7);
+  }
+  return weeknum;
+};
