dee34670473c6d5520d055eb240170503154c9a6
chestnut1044 <wbk1620@gmail.com>
2023-08-23 16:57:18 +0900

    Fix : 카드 컴포넌트

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import styles from "./card.module.css";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaTrash } from "react-icons/fa";
 import { MdThumbUp, MdThumbDown } from "react-icons/md";
+import {
+  TbRosetteNumber1,
+  TbRosetteNumber2,
+  TbRosetteNumber3,
+} from "react-icons/tb";
 
 export default function Card(props) {
-
-  const { type, handleGradeClick, cardData } = props;
+  const { type, handleGradeClick, handleDeleteGallery, cardData, id } = props;
   if (type == "GRADE") {
     return (
-      <card className={styles.card_grade}>
-        <img src={cardData.url} className={styles.grade_img}></img>
-        <info className={styles.grade_btn}>
+      <div className={styles.grade}>
+        <img src={cardData.url} className={styles.card_img}></img>
+        <div className={styles.grade_btn}>
           <button
             className={styles.minus_btn}
             onClick={() => handleGradeClick("down", cardData.id)}
@@ -23,36 +27,61 @@ export default function Card(props) {
           >
             <MdThumbUp />
           </button>
-        </info>
-      </card>
+        </div>
+      </div>
     );
-  }
-  
-  
-  
-  else if (type == "RANK") {
+  } else if (type == "RANK") {
     return (
-      <card className={styles.card_rank}>
-        {/* be에서 받아오기 */}
-        <img
-          src={require("../../img/bg1.jpg")}
-          className={styles.img}
-        ></img>
+      <div className={styles.rank}>
+        <img src={cardData.imgUrl} className={styles.card_img}></img>
         <div className={styles.profile}>
-          <h1>1</h1>
-          <img
-            src={require("../../img/bg1.jpg")}
-            className={styles.profile_img}
-          />
+          <h1 className={styles.profile_rank}>
+            {id == 0 ? (
+              <TbRosetteNumber1 color="#D5A11E" />
+            ) : id == 1 ? (
+              <TbRosetteNumber2 color="#A3A3A3" />
+            ) : (
+              <TbRosetteNumber3 color="#CD7F32" />
+            )}
+          </h1>
+          <img src={cardData.member_img} className={styles.profile_img} />
           <div className={styles.profile_info}>
-            <h3>닉네임</h3>
+            <h3>{cardData.nickname}</h3>
             <div className={styles.profile_text}>
               <FaHeart className={styles.profile_icon} />
-              <p>467</p>
+              <p>{cardData.like}</p>
             </div>
           </div>
         </div>
-      </card>
+      </div>
+    );
+  } else if (type == "MYPAGE") {
+    return (
+      <div className={styles.modal}>
+        <img src={cardData.picture_url} className={styles.card_img}></img>
+        <div className={styles.profile_modal}>
+          <img src={cardData.member_img} className={styles.profile_img} />
+          <div className={styles.profile_info}>
+            <h3>{cardData.nickname}</h3>
+            <div className={styles.profile_text}>
+              <FaHeart className={styles.profile_heart_icon} />
+              <p>{cardData.like}</p>
+            </div>
+          </div>
+          <FaTrash
+            className={styles.profile_trash_icon}
+            onClick={() => handleDeleteGallery()}
+          />
+        </div>
+      </div>
+    );
+  } else if (type == "NONE") {
+    return (
+      <div className={styles.none}>
+        <p>더 이상 평가할 사진이 없네요!</p>
+        <p>사진이 등록될때까지 기다려 주세요</p>
+        <p>☺️</p>
+      </div>
     );
   }
 }
