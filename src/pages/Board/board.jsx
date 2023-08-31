2febb1355252dd53bdea32ff0445fb07b29eb73b
chestnut1044 <wbk1620@gmail.com>
2023-09-01 01:59:49 +0900

    rename : index rename

--- src/pages/Board/board.jsx
+++ src/pages/Board/board.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Header from "../../components/Header/header";
-import Navbar from "../../components/Navbar/navbar";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
 import styles from "./board.module.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -11,14 +11,15 @@ export default function Board() {
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
-    axios.get("/post_list")
-  .then((response) => {
-    console.log(response.data);
-    setBoardList(response.data.reverse()); 
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    axios
+      .get("/post_list")
+      .then((response) => {
+        console.log(response.data);
+        setBoardList(response.data.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   useEffect(() => {
@@ -29,26 +30,27 @@ export default function Board() {
     return () => clearInterval(timer);
   }, []);
 
-  const goWrite=()=>{
-    window.location.href="/BoardWrite"
-  }
+  const goWrite = () => {
+    window.location.href = "/BoardWrite";
+  };
 
-  const time=(e)=>{
-  const timeDifference = currentTime - new Date(e.postTime).getTime()-9 * 60 * 60 * 1000;
-const minutesDifference = Math.floor(timeDifference / (1000 * 60));
-const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
-const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const time = (e) => {
+    const timeDifference =
+      currentTime - new Date(e.postTime).getTime() - 9 * 60 * 60 * 1000;
+    const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+    const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-if(minutesDifference<1){
-  return("방금전")
-}else if (minutesDifference < 60) {
-  return(`${minutesDifference}분전`);
-} else if (hoursDifference < 24) {
-  return(`${hoursDifference}시간전`);
-} else {
-  return(`${daysDifference}일전`);
-}
-  }
+    if (minutesDifference < 1) {
+      return "방금전";
+    } else if (minutesDifference < 60) {
+      return `${minutesDifference}분전`;
+    } else if (hoursDifference < 24) {
+      return `${hoursDifference}시간전`;
+    } else {
+      return `${daysDifference}일전`;
+    }
+  };
   return (
     <div className={styles.board}>
       <Header
@@ -58,7 +60,7 @@ if(minutesDifference<1){
         buttons={<FiEdit />}
       ></Header>
       <div id={styles.wrapper}>
-        {boardList.map((item,idx) => {
+        {boardList.map((item, idx) => {
           return (
             <div id={styles.boardList} key={idx}>
               {item.postImgUrl === null ? (
