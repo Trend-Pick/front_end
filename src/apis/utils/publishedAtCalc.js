d30db76bcd36c058f9b1323edf8ed3fce8d7313e
chestnut1044 <wbk1620@gmail.com>
2023-09-19 14:41:48 +0900

    fix : 발행 시간 동기화

--- src/apis/utils/publishedAtCalc.js
+++ src/apis/utils/publishedAtCalc.js
@@ -6,15 +6,16 @@ export default function publishedAtCalc(n, title) {
   console.log(time);
   // 60초 = 1분 1분보다 작다면 초전으로 표기
   if (time < 60) {
-    return `${time}초전`;
+    return `${Math.floor(time)}초전`;
   }
   // 360초  = 60분 = 1시간 1시간보다 작다면 분전으로 표기
   else if (time < 360) {
-    return `${time / 60}분전`;
+    return `${Math.floor(time / 60)}분전`;
   }
   // 86,400초 = 1440분 = 24시간 = 1일 1일보다 작다면 시간전으로 표기
-  else if (time < 86400) {
-    return `${Math.floor(time / (60 * 60))}시간전`;
+  else if (time < 86460) {
+    if (time / (60 * 60) < 1) return `${Math.floor(time / 60)}분전`;
+    return `${time / (60 * 60)}시간전`;
   }
   //  = 28일보다 작다면 개월전으로 표기
   else if (time < 2419200 / 4) {
